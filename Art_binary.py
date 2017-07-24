864ebfedb6b8e16a950c2f46df00d69e18b45311
aniket3167 <aniket3167@gmail.com>
2017-07-25 02:45:38 +0530

    1st change
    converted the code in python3

--- Art_binary.py
+++ Art_binary.py
@@ -22,9 +22,9 @@ print (df.shape)
 
 clean_train_reviews = []
 num_site = df['URLofSite'].size
-print num_site
+print (num_site)
 
-for index in xrange(0, num_site):
+for index in range(0, num_site):
 	tokens = tokenize(df['URLofSite'][index])
 	clean_train_reviews.append(tokens)
 
@@ -36,13 +36,13 @@ train_data_features = vectorizer.fit_transform(clean_train_reviews)
 train_data_features = train_data_features.toarray()
 
 test = pd.read_csv('test1.csv', header=0)
-print test1.shape
+print (test.shape)
 
 num_site = len(test['URLofSite'])
 test_cat = []
 
-print "\nStarting to make some predictions\n"
-for i in xrange(0, num_site):
+print ("\nStarting to make some predictions\n")
+for i in range(0, num_site):
 	num_site_tok = tokenize(test['URLofSite'][i])
 	test_cat.append(num_site_tok)
 
@@ -53,31 +53,31 @@ clf1 = RandomForestClassifier(n_estimators=30)
 clf1 = clf1.fit(train_data_features, df["Category"])
 result = clf1.predict(test_data_features)
 acc = accuracy_score(test['Category'], result)
-print acc
+print (acc)
 
 
 clf = KNeighborsClassifier(n_neighbors=2)
 clf.fit(train_data_features, df['Category'])
-print "Data is fit finally\n"
+print ("Data is fit finally\n")
 pre2 = clf.predict(test_data_features)
 acc3 = accuracy_score(test['Category'], pre2)
-print acc3
+print (acc3)
 
 clf2 = LogisticRegression()
 clf2.fit(train_data_features, df['Category'])
 pre = clf2.predict(test_data_features)
 acc2 = accuracy_score(test['Category'], pre)
-print acc2
+print (acc2)
 
 #	 		CREATING THE ENSEMBLE 			#
 
-print "Building the Ensemble"
+print ("Building the Ensemble")
 eclf = VotingClassifier(estimators=[
 	('rf', clf1), ('knn', clf), ('lr', clf2)], voting='soft')
 
 eclf = eclf.fit(train_data_features, df['Category'])
 
-print "Predicting Using Ensemble"
+print ("Predicting Using Ensemble")
 fres = eclf.predict(test_data_features)
 facc = accuracy_score(test['Category'], fres)
-print facc
+print (facc)
